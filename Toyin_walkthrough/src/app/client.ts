82dbafbbf7d663c31a299a9dc146c1cd5f52611f
eaoyama <e.h.aoyama@gmail.com>
2024-06-06 01:01:26 -0700

    added toyin class angular project

---
+++ Toyin_walkthrough/src/app/client.ts
@@ -0,0 +1,12 @@
+export class Client {
+
+    constructor(
+        public id: number,
+        public name: string,
+        public address: string,
+        public password: string
+        
+    ){}
+
+
+}
